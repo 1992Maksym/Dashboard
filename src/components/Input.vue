93e85df95022a97228cef2b834316d12a4dd04ae
Максим <maksimsalov8@gmail.com>
2024-06-12 13:19:38 +0300

    create Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "Input",
+  props: {
+    value: String,
+    placeholder: String,
+  },
+  methods: {
+    getValue(event) {
+      this.$emit('update:value',event.target.value);
+    }
+  },
+  emits: ['update:value']
+}
+</script>
+
+<template>
+  <input
+    :placeholder="placeholder"
+    :value="value"
+    @input="getValue"
+  ></input>
+</template>
+
+<style scoped>
+
+</style>
