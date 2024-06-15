d28e8545034a50f4b3d8e17911adff07a3c556a5
Максим <maksimsalov8@gmail.com>
2024-06-15 16:17:36 +0300

    create store file and connect it at main file

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/styles.scss'
+import store from "@/store/index.js";
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
