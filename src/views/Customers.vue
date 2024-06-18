a935d62c9605449a96e63a4580eab9a44f4fadf1
Максим <maksimsalov8@gmail.com>
2024-06-18 18:24:53 +0300

    add footer for customers block

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -76,6 +76,27 @@ export default {
         </div>
 
       </div>
+      <div class="content__footer">
+        <div class="content__footer-info">Showing data 1 to 8 of  256K entries</div>
+        <div class="content__footer-pagination">
+          <div class="pagination__arrow pagination__item">
+            <svg width="5" height="7" viewBox="0 0 5 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2.936 6.392L0.176 3.668L2.936 0.944H4.664L1.892 3.668L4.664 6.392H2.936Z" fill="#404B52"/>
+            </svg>
+          </div>
+          <div class="pagination__page pagination__item active">1</div>
+          <div class="pagination__page pagination__item">2</div>
+          <div class="pagination__page pagination__item">3</div>
+          <div class="pagination__page pagination__item">4</div>
+          <div class="pagination__dots pagination__item">...</div>
+          <div class="pagination__page pagination__item">40</div>
+          <div class="pagination__arrow pagination__item">
+            <svg width="5" height="7" viewBox="0 0 5 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0.344 0.944H2.072L4.832 3.668L2.072 6.392H0.344L3.116 3.668L0.344 0.944Z" fill="#404B52"/>
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -89,7 +110,7 @@ export default {
     height: 100%;
 
     .customers__title {
-      font-size: 32px;
+      font-size: 24px;
       font-weight: 500;
     }
     .main__title {
@@ -110,11 +131,11 @@ export default {
           flex-direction: column;
 
           .content__title {
-            font-size: 30px;
+            font-size: 22px;
             font-weight: 500;
           }
           .content__status {
-            font-size: 18px;
+            font-size: 14px;
             color: #16C098;
           }
         }
@@ -133,13 +154,13 @@ export default {
       .content__body-groups {
         display: grid;
         grid-template-columns: 2fr 1fr 2fr 3fr 2fr 1fr;
-        font-size: 16px;
+        font-size: 14px;
         color: $customers-light-grey;
-        padding: 20px 67px 20px 38px;
+        padding: 20px 44px 20px 38px;
         border-bottom: 1px solid $customers-light-grey;
       }
       .content__body-wrap {
-        margin: 0 67px 0 38px;
+        padding: 0 44px 0 38px;
 
         .content__body-customers {
           display: grid;
@@ -176,6 +197,39 @@ export default {
       }
       .customer__status {
         text-align: center;
+      }
+      .content__footer {
+        display: flex;
+        justify-content: space-between;
+        padding: 30px 40px 40px;
+
+        .content__footer-info {
+          font-size: 14px;
+          color: $customers-light-grey;
+        }
+        .content__footer-pagination {
+          display: flex;
+          justify-content: space-between;
+
+          .pagination__arrow,
+          .pagination__page {
+            padding: 6px 9px;
+            background-color: #f5f5f5;
+            color: #404b52;
+          }
+          .pagination__item {
+            margin-left: 12px
+          }
+          .pagination__dots {
+            display: flex;
+            flex-direction: column;
+            justify-content: end;
+          }
+          .pagination__page.active {
+            background-color: #5932EA;
+            color: #ffffff;
+          }
+        }
       }
     }
   }
